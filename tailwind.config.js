ebba16745236d46ffa44c858e30711bd38070620
Natalia <wasiluk.natalia@gmail.com>
2025-01-29 10:44:00 +0100

    Default shopify setup, first lessons ahead

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layout/**/*.liquid",
+    "./templates/**/*.liquid",
+    "./sections/**/*.liquid",
+    "./snippets/**/*.liquid",
+    "./src/**/*.tsx",
+    "./src/**/*.ts",
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
